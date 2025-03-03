9e4c1aa56aa9005a72356cfad6cad9384c37e4b1
donisetiawan12 <ds5042973@gmail.com>
2025-03-03 11:19:42 +0700

    updt

--- pertemuan_2/praktikum/form.php
+++ pertemuan_2/praktikum/form.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Form | Doni Setiawan </title>
 </head>
 <body>
     <!-- Special version of Bootstrap that only affects content wrapped in .bootstrap-iso -->
